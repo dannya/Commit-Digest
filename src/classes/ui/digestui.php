e968bd97ce830edf100dac6f321a44ce327be15f
Ingo Malchow <imalchow@kde.org>
2013-04-20 19:18:50 +0200

    fix meta viewport, use comma instead of semicolon

--- src/classes/ui/digestui.php
+++ src/classes/ui/digestui.php
@@ -109,7 +109,7 @@ class DigestUi {
               <meta name="description" content="' . Config::$meta['description'] . '" />
               <meta name="keywords" content="' . Config::$meta['keywords'] . '" />
 
-              <meta name="viewport" content="width=device-width; initial-scale=1.0" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
               <link rel="shortcut icon" href="' . BASE_URL .'/favicon.ico" type="image/x-icon" />
               <link rel="icon" href="' . BASE_URL . '/favicon.ico" type="image/x-icon" />
